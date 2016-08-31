2a3ec5fc449abbcb12a14cff121278b0668cea9a
scuper42 <eirikhauge@hotmail.com>
2016-08-31 20:06:51 +0200

    Updated the c++ program to write to file

--- warmup/warmup.cpp
+++ warmup/warmup.cpp
@@ -4,6 +4,7 @@
 #include <math.h>       /* mathfunctions */
 #include <iostream>     /* output */
 #include <fstream>      /* output to file */
+#include <iomanip>      /* pretty, pretty */
 
 using namespace std;
 
@@ -13,15 +14,20 @@ int singleprec()
   float fpluss, fnorm, fmin;
   x = sqrt(2);
   hstop = 1e-20;
+  ofstream myfile;
+  myfile.open("/home/eirik/Documents/Git/FYS4150/warmup/data/ex2cpp.txt");
   for (h = 1; h > hstop; h /= 10)
     { fpluss = atan(x + h);
       fnorm = atan(x);
       fmin = atan(x - h);
       dfdx2 = (fpluss - fnorm)/h;
       dfdx3 = (fpluss - fmin)/(2.0*h);
-      cout << "dfdx2 = "<< dfdx2 << ", ";
-      cout << "dfdx3 = "<< dfdx3 << ", ";
-      cout << "h = "<< h << "\n";
+      cout << "dfdx2 = "<< setw(10) << dfdx2;
+      cout << ", dfdx3 = "<< setw(10) << dfdx3;
+      cout << ", h = "<< setw(10) << h << endl;
+      myfile << "dfdx2 = "<< setw(10) << dfdx2 << ", ";
+      myfile << ", dfdx3 = "<< setw(10) << dfdx3;
+      myfile << ", h = "<< setw(10) << h << endl;
     }
   return 0;
 }
@@ -32,31 +38,33 @@ int doubleprec()
   double fpluss, fnorm, fmin;
   x = sqrt(2);
   hstop = 1e-20;
-  myfile.open("/home/eirik/Documents/Git/FYS4150/warmup/data/ex2cpp.txt");
+  ofstream myfile;
+  myfile.open("/home/eirik/Documents/Git/FYS4150/warmup/data/ex2cpp.txt", std::ios_base::app);
+  myfile << "\n";
   for (h = 1; h > hstop; h /= 10)
     { fpluss = atan(x + h);
       fnorm = atan(x);
       fmin = atan(x - h);
       dfdx2 = (fpluss - fnorm)/h;
       dfdx3 = (fpluss - fmin)/(2.0*h);
-      cout << "dfdx2 = "<< dfdx2 << ", ";
-      cout << "dfdx3 = "<< dfdx3 << ", ";
-      cout << "h = "<< h << "\n";
-      myfile << "dfdx2 = "<< dfdx2 << ", ";
-      myfile << "dfdx3 = "<< dfdx3 << ", ";
-      myfile << "h = "<< h << "\n";
+      cout << "dfdx2 = "<< setw(10) << dfdx2;
+      cout << ", dfdx3 = "<< setw(10) << dfdx3;
+      cout << ", h = "<< setw(10) << h << endl;
+      myfile << "dfdx2 = "<< setw(10) << dfdx2 << ", ";
+      myfile << ", dfdx3 = "<< setw(10) << dfdx3;
+      myfile << ", h = "<< setw(10) << h << endl;
     }
+  myfile.close();
   return 0;
 }
 
 
   int main(int argn, char** argv)
   {
-    ofstream myfile;
+    remove("/home/eirik/Documents/Git/FYS4150/warmup/data/ex2cpp.txt");
     singleprec();
     cout << "\n";
     doubleprec();
-    myfile.close();
     return 0;
   }
   
