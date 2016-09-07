2558b43ae767b080483048aa7ccbf762f83f3b73
scuper42 <eirikhauge@hotmail.com>
2016-09-07 11:30:07 +0200

    Changed from functions to class for importing.

--- project1/test1.py
+++ project1/test1.py
@@ -2,36 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-def hermione(filename, variable1, variable2):
-    """
-    Function that automatically reads a file and
-    makes a plot
-    """
 
-    filename = "%s.txt" %filename
+class Rowling:
+    def hermione(self, filename, variable1 = 0, variable2 = 1):
+        """
+        Function that automatically reads a file and
+        makes a plot
+        """
 
-    x = []
-    y = []
+        filename = "%s.txt" %filename
+
+        x = []
+        y = []
     
-    f = open(filename, "r")
-    f.readline() #skips first line
-    for line in f:
-        data = line.split(',')
-        data[-1] = ''.join(data[-1].split())
-        data[-2] = ''.join(data[-2].split())
-        x.append(float(data[int(variable1)]))
-        y.append(float(data[int(variable2)]))
+        f = open(filename, "r")
+        f.readline() #skips first line
+        for line in f:
+            data = line.split(',')
+            data[-1] = ''.join(data[-1].split())
+            data[-2] = ''.join(data[-2].split())
+            x.append(float(data[int(variable1)]))
+            y.append(float(data[int(variable2)]))
+        return x, y
 
-    return x, y
-
-def harry_plotter(x, y):
-    plt.plot(x, y, label = '%s' %sys.argv[6])
-    plt.xlabel('%s' %sys.argv[4])
-    plt.ylabel('%s' %sys.argv[5])
-    plt.legend()
-    plt.show()
+    def harry_plotter(self, x, y, xlab = 'x', ylab='y'
+                      , plotlab = 'This is your graf',
+                      d=1):
+        plt.plot(x, y, label = '%s' %plotlab)
+        plt.xlabel('%s' %xlab)
+        plt.ylabel('%s' %ylab)
+        plt.legend(loc=d)
+        plt.show()
 
 
 if __name__ == '__main__':
-    x, y = hermione(sys.argv[1], sys.argv[2], sys.argv[3])
-    harry_plotter(x, y)
+    a = Rowling()
+    x, y = a.hermione("ex2", 0, 1)
+    a.harry_plotter(x, y)
