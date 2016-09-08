d78eaa7a0eff991d2d6b07d32108d495b866a7dd
scuper42 <eirikhauge@hotmail.com>
2016-09-08 14:23:31 +0200

    Made folders

---
+++ qttesting/knutsen/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    int number = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    int a = number;
+    return 0;
+}
+
