fdd9f385cc060227be2910b23b24bf3baf512a21
scuper42 <eirikhauge@hotmail.com>
2016-09-09 12:07:07 +0200

    Changed a, b and c in main.cpp from int to double.

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -6,7 +6,7 @@
 #include "functions.h"
 
 using namespace std;
-
+ofstream myfile;
 
 int main(int argc, char *argv[])
 {
@@ -24,9 +24,9 @@ int main(int argc, char *argv[])
     int n = 10;
 
     /* Defining arrays */
-    int *a = new int[n+1];
-    int *b = new int[n+1];
-    int *c = new int[n+1];
+    double *a = new double[n+1];
+    double *b = new double[n+1];
+    double *c = new double[n+1];
 
     double *p = new double[n+1];
     double *v = new double[n+1];
@@ -45,29 +45,43 @@ int main(int argc, char *argv[])
     }
 
     for (int i=0; i < n; i++){
-        a[i] = -1;
-        b[i] = 2;
-        c[i] = -1;
+        a[i] = -1.0;
+        b[i] = 2.0;
+        c[i] = -1.0;
 
     }
 
     /* Filling array p with values */
     source_term(n,h,x,p);
-    for (int i=0; i < n; i++)
+    /*for (int i=0; i < n; i++)
     {
         cout<<"i = "<<i<<" x="<<x[i]<<" p="<<p[i] <<endl;
-    }
+    }*/
 
-    for (int i=1; i < n; i++){
+    for (int i=1; i < n+1; i++){ //n+1 instead?
         p[i] = p[i] - p[i-1]*(a[i-1]/b[i-1]);
         b[i] = b[i] - ((c[i-1]*a[i-1])/b[i-1]);
     }
 
-    v[n+1] = p[n+1]/b[n+1];   /*I believe there is something wrong with my indexing*/
-
-    for (int i=n; i > 0; i--){
+    v[n] = p[n]/b[n];   /*I believe there is something wrong with my indexing*/
+    //cout << "v[n] = " <<v[n] << " p[n] = " << p[n] << " b[n] = " << b[n] << endl;
+    for (int i=n-1; i > -1; i--){
         v[i] = (p[i] - c[i]*v[i+1])/b[i];
+        //cout << v[i] << endl;
     }
-    return 0;
 
+    /*for (int i=0; i<n+1; i++){
+        //cout<<"i = "<<i<< " b[i] = "<< b[i] << " p[i] = "<<p[i]<<endl;
+    }*/
+    // Open file and write the results to file:
+        myfile.open("../textfiles/1b.txt");
+        myfile << setiosflags(ios::showpoint | ios::uppercase);
+        myfile << "       x:          v(x):  " << endl;
+        for (int i=1;i<=n;i++) {
+           myfile << setw(15) << setprecision(8) << x[i] << ",";
+           //ofile << setw(15) << setprecision(8) << u[i];
+           myfile << setw(15) << setprecision(8) << v[i] << endl;
+        }
+        myfile.close();
+    return 0;
 }
