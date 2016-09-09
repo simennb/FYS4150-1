c921806ab520cef9c979caadc9ca34e8dbc6aa64
scuper42 <eirikhauge@hotmail.com>
2016-09-09 14:11:50 +0200

    Changed some of the stopping points of the for loops. Changed the calculation of p and b. Problem: v(x) should end at 0. It does not.

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>      /* printf */
 #include <math.h>       /* mathfunctions */
 #include <iostream>     /* output */
+#include <string>
 #include <fstream>      /* output to file */
 #include <iomanip>      /* pretty, pretty */
 #include "functions.h"
@@ -22,29 +23,30 @@ int main(int argc, char *argv[])
     }*/
 
     int n = 10;
+    string outfile;
 
     /* Defining arrays */
-    double *a = new double[n+1];
-    double *b = new double[n+1];
-    double *c = new double[n+1];
+    double *a = new double[n+2];
+    double *b = new double[n+2];
+    double *c = new double[n+2];
 
-    double *p = new double[n+1];
-    double *v = new double[n+1];
+    double *p = new double[n+2];
+    double *v = new double[n+2];
 
-    double *u = new double[n+1]; /* for exact solution */
+    double *u = new double[n+2]; /* for exact solution */
 
     /* since p = h^2*f, its probably better computationally to just do multiply by h in the function for f(x)
     double *f = new double[n+1];
     */
 
     double *x = new double[n+2];
-    double h = 1.0/(n+1.0);
+    double h = 1.0/(n);
 
-    for (int i=0; i< n+1; i++){
+    for (int i=0; i<= n+1; i++){
         x[i] = i*h;
     }
 
-    for (int i=0; i < n; i++){
+    for (int i=0; i < n+1; i++){
         a[i] = -1.0;
         b[i] = 2.0;
         c[i] = -1.0;
@@ -53,33 +55,28 @@ int main(int argc, char *argv[])
 
     /* Filling array p with values */
     source_term(n,h,x,p);
-    /*for (int i=0; i < n; i++)
-    {
-        cout<<"i = "<<i<<" x="<<x[i]<<" p="<<p[i] <<endl;
-    }*/
 
-    for (int i=1; i < n+1; i++){ //n+1 instead?
-        p[i] = p[i] - p[i-1]*(a[i-1]/b[i-1]);
-        b[i] = b[i] - ((c[i-1]*a[i-1])/b[i-1]);
+    for (int i=1; i <= n; i++){ //n+1 instead?
+        p[i] = p[i] - p[i-1]*(a[i]/b[i-1]);
+        b[i] = b[i] - ((c[i-1]*a[i])/b[i-1]);
+        u[i] = 1.0 - (1.0 - exp(-10.0))*x[i] - exp(-10.0*x[i]);
     }
+    cout << u[0] << endl;
 
     v[n] = p[n]/b[n];   /*I believe there is something wrong with my indexing*/
-    //cout << "v[n] = " <<v[n] << " p[n] = " << p[n] << " b[n] = " << b[n] << endl;
-    for (int i=n-1; i > -1; i--){
+    for (int i=n-1; i >= 1; i--){
         v[i] = (p[i] - c[i]*v[i+1])/b[i];
-        //cout << v[i] << endl;
     }
 
-    /*for (int i=0; i<n+1; i++){
-        //cout<<"i = "<<i<< " b[i] = "<< b[i] << " p[i] = "<<p[i]<<endl;
-    }*/
+    outfile = "../textfiles/1bN" + to_string(n) + ".txt";
+
     // Open file and write the results to file:
-        myfile.open("../textfiles/1b.txt");
+        myfile.open(outfile);
         myfile << setiosflags(ios::showpoint | ios::uppercase);
-        myfile << "       x:          v(x):  " << endl;
-        for (int i=1;i<=n;i++) {
+        myfile << "       x:          u(x):          v(x):  " << endl;
+        for (int i=0;i<=n;i++) {
            myfile << setw(15) << setprecision(8) << x[i] << ",";
-           //ofile << setw(15) << setprecision(8) << u[i];
+           myfile << setw(15) << setprecision(8) << u[i] << ",";
            myfile << setw(15) << setprecision(8) << v[i] << endl;
         }
         myfile.close();
